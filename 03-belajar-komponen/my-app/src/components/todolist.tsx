202a03d829a66814b75a47da6251d1692b45e6c9
DESKTOP-9VJCIF5\nayla <alfarobi695@gmail.com>
2024-02-29 13:16:21 +0700

    W03: Jawaban soal 7

--- 03-belajar-komponen/my-app/src/components/todolist.tsx
+++ 03-belajar-komponen/my-app/src/components/todolist.tsx
@@ -1,3 +1,57 @@
+// const person = {
+//     name: 'Gregorio Y. Zara',
+//     theme: {
+//         backgroundColor: 'black',
+//         color: 'pink'
+//     }
+// };
+
+// export default function TodoList() {
+//     return (
+//         <div style={person.theme}>
+//             <h1>{person.name}'s Todos</h1>
+//             <img
+//                 className="avatar"
+//                 src="https://i.imgur.com/7vQD0fPs.jpg"
+//                 alt="Gregorio Y. Zara"
+//             />
+//             <ul>
+//                 <li>Improve the videophone</li>
+//                 <li>Prepare aeronautics lectures</li>
+//                 <li>Work on the alcohol-fuelled engine</li>
+//             </ul>
+//         </div>
+//     );
+// }
+
+// const person = {
+//     name: 'Gregorio Y. Zara',
+//     theme: {
+//         backgroundColor: 'black',
+//         color: 'pink'
+//     },
+//     avatar: 'https://i.imgur.com/7vQD0fPs.jpg' // Menambahkan properti avatar dengan URL gambar
+// };
+
+// export default function TodoList() {
+//     return (
+//         <div style={person.theme}>
+//             <h1>{person.name}'s Todos</h1>
+//             <img
+//                 className="avatar"
+//                 src={person.avatar} // Menggunakan URL gambar dari properti avatar
+//                 alt="Gregorio Y. Zara"
+//             />
+//             <ul>
+//                 <li>Improve the videophone</li>
+//                 <li>Prepare aeronautics lectures</li>
+//                 <li>Work on the alcohol-fuelled engine</li>
+//             </ul>
+//         </div>
+//     );
+// }
+
+
 const baseUrl = 'https://i.imgur.com/';
 const person = {
     name: 'Gregorio Y. Zara',
