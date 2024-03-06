717315701ca351fc89e3b9f3fff26ce3f22f9d50
DESKTOP-9VJCIF5\nayla <alfarobi695@gmail.com>
2024-03-06 10:49:12 +0700

    Praktikum 3

--- 04-event-state/src/app/page.tsx
+++ 04-event-state/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Tombol_1, { Tombol_2 } from "@/components/button";
+import Tombol_1, { Tombol_2, Tombol_3 } from "@/components/button";
 
 export default function Home() {
   return (
@@ -9,6 +9,11 @@ export default function Home() {
         <Tombol_1 />
         <hr></hr>
         <Tombol_2 isiPesan="Tidak ada pesan" namaTombol="Pesan" />
+      </div>
+      <br /><br />
+      <div className="bg-red-500 onClic={()=>alert('Parent Element : Div')} ">
+        <Tombol_3 isiPesan="Child Element : Tombol-1" namaTombol="Pesan"/>
+        <Tombol_3 isiPesan="Child Element : Tombol-2" namaTombol="Pesan"/>
       </div>
     </>
   );
