72a9a4261b5b6a993ee34b3dbef1892f68908b1a
DESKTOP-9VJCIF5\nayla <alfarobi695@gmail.com>
2024-03-08 01:05:32 +0700

    Jawaban Soal Praktikum 4

--- 04-event-state/src/components/gallery.tsx
+++ 04-event-state/src/components/gallery.tsx
@@ -1,24 +1,63 @@
+// import { sculptureList } from "@/data/article";
+// import { useState } from 'react';
+// export default function Gallery() {
+//     const [index, setIndex] = useState(0);
+//     function handleClick() {
+//         setIndex(index + 1);
+//     }
+//     let sclupture = sculptureList[index];
+//     return (
+//         <> 
+//             <button
+//                 onClick={handleClick}
+//                 className="bg-blue-500 hover:bg-blue-700 p-2 rounded ">
+//                 Artikel Selanjutnya
+//             </button>
+//             <h2><i>{sclupture.name}</i> oleh {sclupture.artist}</h2>
+//             <h3>({index + 1} dari {sclupture.length})</h3>
+//             <img src={sclupture.url} alt={sclupture.alt} />
+//             <p>
+//                 {sclupture.description}
+//             </p>
+//         </>
+//     )
+// }
+
 import { sculptureList } from "@/data/article";
 import { useState } from 'react';
+
 export default function Gallery() {
     const [index, setIndex] = useState(0);
-    function handleClick() {
-        setIndex(index + 1);
+
+    function handleClickNext() {
+        setIndex(prevIndex => (prevIndex + 1) % sculptureList.length);
     }
-    let sclupture = sculptureList[index];
+
+    function handleClickPrevious() {
+        setIndex(prevIndex => (prevIndex - 1 + sculptureList.length) % sculptureList.length);
+    }
+
+    const sculpture = sculptureList[index];
+
     return (
-        <>
+        <> 
             <button
-                onClick={handleClick}
-                className="bg-blue-500 hover:bg-blue-700 p-2 rounded ">
+                onClick={handleClickPrevious}
+                className="bg-blue-500 hover:bg-blue-700 p-2 rounded mr-2">
+                Artikel Sebelumnya
+            </button>
+            <button
+                onClick={handleClickNext}
+                className="bg-blue-500 hover:bg-blue-700 p-2 rounded">
                 Artikel Selanjutnya
             </button>
-            <h2><i>{sclupture.name}</i> oleh {sclupture.artist}</h2>
-            <h3>({index + 1} dari {sclupture.length})</h3>
-            <img src={sclupture.url} alt={sclupture.alt} />
+            <h2><i>{sculpture.name}</i> oleh {sculpture.artist}</h2>
+            <h3>({index + 1} dari {sculptureList.length})</h3>
+            <img src={sculpture.url} alt={sculpture.alt} />
             <p>
-                {sclupture.description}
+                {sculpture.description}
             </p>
         </>
     )
 }
+
