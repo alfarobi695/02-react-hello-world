3bf1988d4103e9e4c49abab277810b163636ca72
DESKTOP-9VJCIF5\nayla <alfarobi695@gmail.com>
2024-03-13 10:52:29 +0700

    Praktikum 2

---
+++ routing-demo/src/app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import FirstBlog from "./first/page";
+import SecondBlog from "./second/page";
+
+export default function Blog() {
+    return (
+        <>
+        <h1>Blog</h1>
+        <FirstBlog/>
+        <SecondBlog/>
+        </>
+    );
+}
--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "./about/page";
 import Profile from "./about/profile";
+import Blog from "./blogs/page";
 
 export default function Home() {
     return  (
@@ -7,6 +8,7 @@ export default function Home() {
         <h1>Welcome to Home</h1>
         <About/>
         <Profile/>
+        <Blog/>
         </>
     );
 }
