cc8387a496a4175e609b6eac68b71e68a5ba85e2
DESKTOP-9VJCIF5\nayla <alfarobi695@gmail.com>
2024-03-13 10:30:42 +0700

    Praktikum 1

---
+++ routing-demo/src/app/about/profile.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+    return (
+        <>
+        <h1>Biodata : </h1>
+        <h2>Nama  : Muhamad Al Faroby</h2>
+        <h2>NIM   : 2341727001</h2>
+        </>
+    );
+}
