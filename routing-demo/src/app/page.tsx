9d98572bfc9e6b09aa7d4bc632c3d999549f3d31
DESKTOP-9VJCIF5\nayla <alfarobi695@gmail.com>
2024-03-15 00:11:34 +0700

    Praktikum 4

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,16 +1,22 @@
 import About from "./about/page";
-import Profile from "./about/profile";
+import Profile from "./profile/page";
 import Blog from "./blogs/page";
 import ProductDetails from "./products/page";
+import Link from "next/link";
 
 export default function Home() {
-    return  (
+    return (
         <>
-        {/* <h1>Welcome to Home</h1>
-        <About/>
-        <Profile/>
-        <Blog/> */}
-        <ProductDetails/>
+            {/* <h1>Welcome to Home</h1>
+            <About />
+            <Profile />
+            <Blog />
+            <ProductDetails /> */}
+            <h1>Welcome to Home</h1>
+            <Link href='/profile'>Profile</Link><br />
+            <Link href='/about'>About</Link><br />
+            <Link href='/blogs'>Blogs</Link><br />
+            <Link href='/products'>Products</Link>
         </>
     );
 }
